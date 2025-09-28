d31b9e49fc736dea63d9ac9d9d61db7c1ead3fac
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2025-09-29 00:30:35 +0530

    methods

--- 21star&numpattern.py
+++ 21star&numpattern.py
@@ -20,29 +20,37 @@
 #         print(j,end=" ")
 #     print()
 
-
+######################
 # Python program to print the pattern using 'a'
-
-
 #
 # # Top half (rows 1 to 4)
-# for i in range(1, 5):
-#     for j in range(1, 5):
-#         if j <= i:
-#             print("a", end=" ")
-#         else:
-#             print(" ", end=" ")
-#     print()
+for i in range(1, 5):
+    for j in range(1, 5):
+        if j <= i:
+            print("a", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+# Bottom half (rows 5 to 7)
+for i in range(1, 4):
+    for j in range(1, 5):
+        if j <= 4 - i:
+            print("a", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
 #
-# # Bottom half (rows 5 to 7)
-# for i in range(1, 4):
-#     for j in range(1, 5):
-#         if j <= 4 - i:
-#             print("a", end=" ")
+#or
+# for i in range (1,8):
+#     for j in range (1,5):
+#         if i<=4 and j<=i:
+#             print("A",end=" ")
+#         elif i>=5 and j<=8-i:
+#             print("A",end=" ")
 #         else:
-#             print(" ", end=" ")
+#             print(" ",end=" ")
 #     print()
-#
 # a
 # a a
 # a a a
@@ -50,7 +58,7 @@
 # a a a
 # a a
 # a
-
+#############################################################
 # for i in range(1,6):
 #     for j in range(1,6):
 #         if i<=3 and j>=4-i and j<=2+i:
@@ -83,3 +91,4 @@
 # 1 2 3 4 5
 #   2 3 4
 #     3
+##########################################################
