88f75eb92c96153085f07287c7712ec489de2faf
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2025-10-09 01:51:00 +0530

    new

--- 23.py
+++ 23.py
@@ -1,1 +1,14 @@
-fjkg
+for i in range(1, 4):
+        for j in range(1, 6):
+            if j >= 4 - i and j <= 2 + i:
+                print(j, end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+for i in range(1, 3):
+        for j in range(1, 6):
+            if j >= i+1 and j <= 5 - i:
+                 print(j, end=" ")
+            else:
+                print(" ", end=" ")
+        print()
